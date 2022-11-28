ac47dfac0c74fa46138a3c43a9dda9d9ad2fd649
MeanStudent <kh3074@cumc.columbia.edu>
2022-11-28 00:44:23 -0500

    Assignments_revised

---
+++ Assignment3/pyimagesearch/config.py
@@ -0,0 +1,31 @@
+# import the necessary packages
+import os
+
+# initialize the list of class label names
+CLASSES = [
+    "top",
+    "trouser",
+    "pullover",
+    "dress",
+    "coat",
+    "sandal",
+    "shirt",
+    "sneaker",
+    "bag",
+    "ankle boot",
+]
+
+# define the minimum learning rate, maximum learning rate, batch size,
+# step size, CLR method, and number of epochs
+MIN_LR = 1e-9
+MAX_LR = 10e1
+BATCH_SIZE = 64
+STEP_SIZE = 10
+CLR_METHOD = "triangular"
+NUM_EPOCHS = 5
+
+# define the path to the output training history plot and cyclical
+# learning rate plot
+LRFIND_PLOT_PATH = os.path.sep.join(["output", "lrfind_plot.png"])
+TRAINING_PLOT_PATH = os.path.sep.join(["output", "training_plot.png"])
+CLR_PLOT_PATH = os.path.sep.join(["output", "clr_plot.png"])
